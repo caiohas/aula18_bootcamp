1025f6996bdb2eb426d70b4cf6aee5cf8e72849d
Caio Souza <caiohas.2908@gmail.com>
2025-04-08 21:33:22 -0300

    testando contrato de dados

--- exemplo_requests.py
+++ exemplo_requests.py
@@ -1,8 +1,6 @@
 import requests
 from pydantic import BaseModel
 
-url = "https://pokeapi.co/api/v2/pokemon/25"
-
 class PokemonSchema(BaseModel): # contrato de dados, schema de dados, a view da minha API
     name: str
     type: str
@@ -10,11 +8,20 @@ class PokemonSchema(BaseModel): # contrato de dados, schema de dados, a view da 
     class Config:
         orm_mode = True
 
-response = requests.get(url)
-data = response.json()
-data_types = data['types']
-types_list = []
-for type_info in data_types:
-    types_list.append(type_info['type']['name'])
-types = ', '.join(types_list)
-print(data['name'], types)
+def capturar_pokemon(id: int) -> PokemonSchema:
+    url = f"https://pokeapi.co/api/v2/pokemon/{id}"
+    response = requests.get(url)
+    data = response.json()
+    data_types = data['types']
+    types_list = []
+    for type_info in data_types:
+        types_list.append(type_info['type']['name'])
+    types = ', '.join(types_list)
+    return PokemonSchema(name=data['name'], type=types)
+
+if __name__ == "__main__":
+    print(capturar_pokemon(1))
+    print(capturar_pokemon(8))
+    print(capturar_pokemon(25))
+    print(capturar_pokemon(95))
+    print(capturar_pokemon(110))
