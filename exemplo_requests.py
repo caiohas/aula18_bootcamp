cede12dc50f4f0c86cecf23d276682b26bc8860b
Caio Souza <caiohas.2908@gmail.com>
2025-04-08 21:24:48 -0300

    criação do schema

--- exemplo_requests.py
+++ exemplo_requests.py
@@ -1,6 +1,20 @@
 import requests
+from pydantic import BaseModel
 
-url = "https://pokeapi.co/api/v2/pokemon/15"
+url = "https://pokeapi.co/api/v2/pokemon/25"
+
+class PokemonSchema(BaseModel): # contrato de dados, schema de dados, a view da minha API
+    name: str
+    type: str
+
+    class Config:
+        orm_mode = True
+
 response = requests.get(url)
 data = response.json()
-print(data)
+data_types = data['types']
+types_list = []
+for type_info in data_types:
+    types_list.append(type_info['type']['name'])
+types = ', '.join(types_list)
+print(data['name'], types)
